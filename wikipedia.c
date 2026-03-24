f056696d3a9e327d95f4f40ea4500a76c91d112b
Avaash Bhattarai <bhattara@grinnell.edu>
2026-03-24 16:26:50 -0500

    Revert "tried fixing // issue"
    This reverts commit 26e63dc0fb266573303b95e4c90d11d1306b518e.

--- wikipedia.c
+++ wikipedia.c
@@ -2,105 +2,68 @@
 
 #include "wikipedia.h"
 
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+// Output is guaranteed to be written at this location after Python call
+static char* OUTPUT_FILE = ".local.output.txt";
 
-static char script_path[PATH_MAX];
-static char output_path[PATH_MAX];
-
-/* dir + name without "//" when dir is "/" or ends with '/'. */
-static void join_dir_file(char *out, size_t outsz, const char *dir,
-                          const char *name) {
-  const char *sep =
-      (dir[0] != '\0' && dir[strlen(dir) - 1] != '/') ? "/" : "";
-  snprintf(out, outsz, "%s%s%s", dir, sep, name);
-}
-
-static void wiki_paths_ensure(void) {
-  static int ready;
-  if (ready) {
-    return;
-  }
-  ready = 1;
-
-  char dir[PATH_MAX];
-  const char *env = getenv("WIKI_FS_HOME");
-  if (env != NULL && env[0] != '\0') {
-    if (realpath(env, dir) == NULL) {
-      strncpy(dir, env, sizeof(dir) - 1);
-      dir[sizeof(dir) - 1] = '\0';
-    }
-  } else if (getcwd(dir, sizeof(dir)) == NULL) {
-    perror("wiki_paths_ensure: getcwd");
-    exit(1);
-  }
-
-  join_dir_file(script_path, sizeof(script_path), dir, "wikipedia.py");
-  join_dir_file(output_path, sizeof(output_path), dir, ".local.output.txt");
-}
 
 // Returns the contents of the output file
 // Must be freed by caller
-char *read_output_file() {
-  wiki_paths_ensure();
-  FILE *op = fopen(output_path, "r+");
-  if (op == NULL) {
-    // file does not exist (request was unsuccessful)
-    return NULL;
-  }
+char* read_output_file() {
+    FILE* op = fopen(OUTPUT_FILE, "r+");
+    if (op == NULL) { 
+        // file does not exist (request was unsuccessful)    
+        return NULL;
+    }
 
-  int content_sz = 0;
-  do {
-    content_sz += 1;
-  } while (fgetc(op) != EOF);
+    int content_sz = 0;
+    do {
+        content_sz += 1;
+    } while (fgetc(op) != EOF);
 
-  char *content = malloc(content_sz);
-  fseek(op, 0, SEEK_SET);
+    char* content = malloc(content_sz);
+    fseek(op, 0, SEEK_SET);
+    
+    int index = 0;
+    do {
+        content[index++] = fgetc(op);
+    } while (index < content_sz);
 
-  int index = 0;
-  do {
-    content[index++] = fgetc(op);
-  } while (index < content_sz);
-
-  content[index] = '\0';
-  return content;
+    content[index] = '\0';
+    return content;
 }
 
-char *get_root() {
-  wiki_paths_ensure();
-  char *args[] = {"python3", script_path, "-ft", NULL};
-
-  if (execvp("python3", args)) {
-    perror("execvp failed: get_root");
-    exit(2);
-  }
-
-  return read_output_file();
+char* get_root() {
+    // root is always the featured title (ft) from Wikipedia
+    char *args[] = { "python3", "wikipedia.py", "-ft", NULL};
+    
+    if(execvp("python3", args)) {
+        perror("execvp failed: get_root");
+        exit(2);
+    }
+    
+    return read_output_file();
 }
 
-char *get_dirs(char *curr_dir) {
-  wiki_paths_ensure();
-  char *args[] = {"python3", script_path, "-gl", curr_dir, NULL};
-
-  if (execvp("python3", args)) {
-    perror("execvp failed: get_dirs");
-    exit(2);
-  }
-
-  return read_output_file();
+char* get_dirs(char* curr_dir) {
+    // get links contained in the page for this dir name
+    char *args[] = { "python3", "wikipedia.py", "-gl", curr_dir, NULL };
+    
+    if(execvp("python3", args)) {
+        perror("execvp failed: get_dirs");
+        exit(2);
+    }
+    
+    return read_output_file();
 }
 
-char *get_content(char *curr_dir) {
-  wiki_paths_ensure();
-  char *args[] = {"python3", script_path, "-gs", curr_dir, NULL};
-
-  if (execvp("python3", args)) {
-    perror("execvp failed: get_content");
-    exit(2);
-  }
-
-  return read_output_file();
+char* get_content(char* curr_dir) {
+    // get wikipedia summary of the page for this dir name
+    char *args[] = { "python3", "wikipedia.py", "-gs", curr_dir, NULL};
+    
+    if(execvp("python3", args)) {
+        perror("execvp failed: get_content");
+        exit(2);
+    }
+    
+    return read_output_file();
 }
